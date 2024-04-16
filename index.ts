85ea170676535b7ebb69f862fa449ecf154db94c
dapp-sculptor <hugodevpro@outlook.com>
2024-04-16 20:21:08 +0900

    udpate: wrap all feature

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// import * as XLSX from 'xlsx';
+import * as XLSX from 'xlsx';
 import fs from 'fs';
 
 interface Info {
@@ -10,18 +10,38 @@ interface Info {
                 time: Date
                 level: number,
             }
-        ]
-        claimable: {
+        ],
+        amount: {
             bronze: number,
             silver: number,
             gold: number,
             diamond: number
-        }
+        },
+        claimable: boolean
     }
 }
 
 let userInfo: Info = {}
 let endTime: Date = new Date()
+
+const fetchExcel = async () => {
+    const workbook: XLSX.WorkBook = XLSX.readFile('info.xlsx'); // Replace 'example.xlsx' with your file name
+
+    // Assuming only one sheet in the workbook
+    const sheetName: string = workbook.SheetNames[0];
+    const worksheet: XLSX.WorkSheet = workbook.Sheets[sheetName];
+
+    // Convert the worksheet to a JSON object
+    const data: any[] = XLSX.utils.sheet_to_json(worksheet);
+    const dataJson = JSON.stringify(data, null, 4);
+    fs.writeFile('data.json', dataJson, (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log(`wrote file data.json`);
+        }
+    })
+}
 
 const start = async () => {
     const data = JSON.parse(fs.readFileSync("data.json", `utf8`))
@@ -34,12 +54,12 @@ const start = async () => {
 
         if (!(address in userInfo)) {
             userInfo[address] = {
-                token: [{ num: 0, time: new Date(), level: 0 }], claimable: {
+                token: [{ num: 0, time: new Date(), level: 0 }], amount: {
                     bronze: 0,
                     silver: 0,
                     gold: 0,
                     diamond: 0
-                }
+                }, claimable: true
             }
         }
 
@@ -73,16 +93,16 @@ const start = async () => {
                     const duration = Math.floor(((new Date(timestamp)).getTime() - (new Date(item.time)).getTime()) / (86400 * 7000))
                     switch (item.level) {
                         case 0:
-                            userInfo[address].claimable.bronze += duration
+                            userInfo[address].amount.bronze += duration
                             break
                         case 1:
-                            userInfo[address].claimable.silver += duration
+                            userInfo[address].amount.silver += duration
                             break
                         case 2:
-                            userInfo[address].claimable.gold += duration
+                            userInfo[address].amount.gold += duration
                             break
                         case 3:
-                            userInfo[address].claimable.diamond += duration
+                            userInfo[address].amount.diamond += duration
                             break
                     }
                 }
@@ -97,42 +117,43 @@ const start = async () => {
     return
 }
 
-start()
+const finish = async () => {
+    for (const [address, details] of Object.entries(userInfo)) {
+        details.token.map((val: {
+            num: number;
+            time: Date;
+            level: number;
+        }, idx: number) => {
+            if (val.num > 0) {
+                const duration = Math.floor(((new Date(endTime)).getTime() - (new Date(val.time)).getTime()) / (86400 * 7000))
 
-console.log(endTime)
-
-for (const [address, details] of Object.entries(userInfo)) {
-    details.token.map((val: {
-        num: number;
-        time: Date;
-        level: number;
-    }, idx: number) => {
-        if (val.num > 0) {
-            const duration = Math.floor(((new Date(endTime)).getTime() - (new Date(val.time)).getTime()) / (86400 * 7000))
-
-            switch (val.level) {
-                case 0:
-                    userInfo[address].claimable.bronze += duration
-                    break
-                case 1:
-                    userInfo[address].claimable.silver += duration
-                    break
-                case 2:
-                    userInfo[address].claimable.gold += duration
-                    break
-                case 3:
-                    userInfo[address].claimable.diamond += duration
-                    break
+                switch (val.level) {
+                    case 0:
+                        userInfo[address].amount.bronze += duration
+                        break
+                    case 1:
+                        userInfo[address].amount.silver += duration
+                        break
+                    case 2:
+                        userInfo[address].amount.gold += duration
+                        break
+                    case 3:
+                        userInfo[address].amount.diamond += duration
+                        break
+                }
             }
+        })
+    }
+
+    const dataJson = JSON.stringify(userInfo, null, 4);
+    fs.writeFile('res.json', dataJson, (err) => {
+        if (err) {
+            console.log('Error writing file:', err);
+        } else {
+            console.log(`wrote file res.json`);
         }
     })
 }
 
-const dataJson = JSON.stringify(userInfo, null, 4);
-fs.writeFile('res.json', dataJson, (err) => {
-    if (err) {
-        console.log('Error writing file:', err);
-    } else {
-        console.log(`wrote file res.json`);
-    }
-})
+start()
+finish()
